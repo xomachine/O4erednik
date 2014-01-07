a912f53588085f048b0b3fdbfd21b61cadb66072
xomachine <xomachiner@gmail.com>
2014-01-07 18:52:38 +0400

    build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Script to build queuer executable
+
+nuitka --exe --recurse-all --recurse-directory=/usr/lib/python3.3/site-packages/PyQt4\
+--show-progress -j 3 --lto --python-version=3.3 --improved --output-dir=./linux_$(uname -m)\
+--remove-output ./queuer.py
+ 
