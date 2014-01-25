203be064c03284208fae984c0ef2f998b6bf11fb
xomachine <xomachiner@gmail.com>
2014-01-25 21:57:46 +0400

    Logable threads support

---
+++ api.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from threading import Thread
+from logging import exception
+
+
+class LoggableThread(Thread):
+
+    def __init__(self):
+        super(LoggableThread, self).__init__()
+        self._real_run = self.run
+        self.run = self._wrap_run
+
+    def _wrap_run(self):
+        try:
+            self._real_run()
+        except:
+            exception('Uncaught exception was occured!')
