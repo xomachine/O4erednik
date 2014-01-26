4ac241f4fafb5742a6fedb0e90ef4d341076456f
xomachine <xomachiner@gmail.com>
2014-01-26 19:29:53 +0400

    changed main file in build script

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 
 nuitka --exe --recurse-all --recurse-directory=/usr/lib/python3.3/site-packages/PyQt4 \
 --show-progress -j 3 --lto --python-version=3.3 --improved --output-dir=./linux_$(uname -m) \
---remove-output ./queuer.py
+--remove-output ./__init__.py
  
