8e4cce44a3fc8a85142040aa9c8c0fb908e01a22
xomachine <xomachiner@gmail.com>
2014-01-30 15:01:01 +0400

    GUI separated from shared, shared separated from UDPServer

--- __init__.py
+++ __init__.py
@@ -1,11 +1,27 @@
 # -*- coding: utf-8 -*-
 
 from core import UDPServer
+from shared import Resources
+from logging import warning
 
 if __name__ == '__main__':
-    # Starting app
-    mainsrv = UDPServer()
+    # Prepare resources
+    shared = Resources()
+    # Try to prepare GUI
+    try:
+        from gui import Backend
+    except ImportError:
+        warning('GUI cann\'t be imported! Running without it.')
+        guibackend = None
+    else:
+        guibackend = Backend()
+        # Allow GUI to send commands
+        guibackend.sendto = shared.udpsocket.sendto
+        # Allow GUI to be informed about changes in queue
+        shared.inform = guibackend.signal
+    # Start app
+    mainsrv = UDPServer(shared)
     mainsrv.start()
-    #TODO: Separate GUI from UDPServer
-    mainsrv.shared.backend.run()
+    if guibackend:
+        guibackend.run()
     mainsrv.join()
