8d3345b0e60c9bbea76f1416daa408fec30ee910
xomachine <xomachiner@gmail.com>
2014-02-01 00:54:36 +0400

    Leftclick menu, "add" signal (frontend side)

--- gui.py
+++ gui.py
@@ -1,12 +1,39 @@
 # -*- coding: utf-8 -*-
 
 from PyQt4.QtGui import QApplication, QSystemTrayIcon, QIcon, QPixmap, QMenu
-from PyQt4.QtCore import QTextCodec
+from PyQt4.QtGui import QCursor, QToolTip
+from PyQt4.QtCore import QTextCodec, SIGNAL
 from os import _exit
 from logging import debug
 import icons
 
 _icons = dict()
+
+
+class LeftMenu(QMenu):
+
+    def __init__(self):
+        super(LeftMenu, self).__init__()
+
+        # Fill elements of menu
+        self.addAction(
+            _icons['add'],
+            self.tr('Add assignment')
+            ).triggered.connect(self.DoAdd)
+
+        self.addSeparator()
+
+        self.local = self.addMenu(
+            _icons['run'],
+            self.tr('Local assignments')
+            )
+        self.remote = self.addMenu(
+            _icons['remote'],
+            self.tr('Remote assignments')
+            )
+
+    def DoAdd(self):
+        pass
 
 
 class RightMenu(QMenu):
@@ -26,8 +53,26 @@ class TrayIcon(QSystemTrayIcon):
     def __init__(self):
         super(TrayIcon, self).__init__()
 
+        # Create right and left click menus
         self.rmenu = RightMenu()
+        self.lmenu = LeftMenu()
+
         self.setContextMenu(self.rmenu)
+        self.activated.connect(self.DoActivate)
+
+        # Connect signals
+        # self.connect(self, SIGNAL(), self.sigfunc)
+        self.connect(self, SIGNAL('add(QString, QString)'), self.SigAdd)
+
+    def DoActivate(self, reason):
+        if reason == self.Trigger:
+            self.lmenu.exec_(QCursor.pos())
+
+    def SigAdd(self, name, tooltip):
+        elem = self.lmenu.local.addMenu(_icons['wait'], name)
+        elem.setToolTip(tooltip)
+        elem.enterEvent = lambda x: QToolTip.showText(QCursor.pos(), name)
+        elem.leaveEvent = lambda x: QToolTip.hideText()
 
 
 class Backend():
@@ -62,4 +107,7 @@ class Backend():
         return QIcon(p)
 
     def signal(self, *signal):
-        pass
+        self._tray.emit(
+            SIGNAL('add(QString, QString)'),
+            'testname',
+            'testtooltip')
