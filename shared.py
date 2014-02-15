a51530dc03a0cba6ed3de9b2e683d99690183977
xomachine <xomachiner@gmail.com>
2014-02-15 16:27:16 +0400

    Licensed under GPLv3

--- shared.py
+++ shared.py
@@ -1,4 +1,23 @@
 # -*- coding: utf-8 -*-
+'''
+    This file is part of O4erednik.
+
+    O4erednik is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2014 Fomichev Dmitriy
+'''
+
 
 from threading import Lock, Event
 from socket import socket, AF_INET, SOCK_DGRAM, IPPROTO_UDP, if_nameindex
