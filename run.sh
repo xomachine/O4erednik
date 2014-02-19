e3513f3fda14b9b9e3186d391c071fb15aba293c
xomachine <xomachiner@gmail.com>
2014-02-19 11:42:04 +0400

    fixed run.sh for using with symlinks

--- run.sh
+++ run.sh
@@ -24,4 +24,5 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+cd $DIR
 exec python3 $DIR/__init__.py
