4f27037c6853d5af13c87fd1a73e98f824fa0105
xomachine <xomachiner@gmail.com>
2014-03-12 15:38:14 +0400

    strange commit

--- modules/gamess.py
+++ modules/gamess.py
@@ -44,7 +44,7 @@ class Module():
                 error('''GAMESS jobs will not be executed!
 Please set kernel.shmmax>=44498944 and restart me!
 E.g.: sudo sysctl -w kernel.shmmax=6269961216''')
-                self.register = lambda x: False
+                raise
         environ['GMSPATH'] = self.gmspath
         environ['SCR'] = self.tmp
 
