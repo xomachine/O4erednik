712ae9014cd7850589991f9f79acbbd3e831422d
xomachine <xomachiner@gmail.com>
2014-08-31 17:04:51 +0400

    Stub for handling unexpected answers in node allocation

--- core.py
+++ core.py
@@ -348,6 +348,8 @@ class UDPServer(LogableThread):
         debug("Found:" + str(allocated))
         for node, nproc in allocated:
             if procs < nprocs:
+                if (not nproc is int) or (not node is str):
+                    continue
                 debug("Asking for " + node)
                 procs += nproc
                 self.udp.sendto(
